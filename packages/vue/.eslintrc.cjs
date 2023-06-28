7fe208d21b448b376fdb93da0ec8664a994c6221
汪心禾 <wangxinhe06@gmail.com>
2023-06-29 02:19:02 +0800

    @vue/eslint-config-airbnb

--- packages/vue/.eslintrc.cjs
+++ packages/vue/.eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
+    '@vue/eslint-config-airbnb',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
